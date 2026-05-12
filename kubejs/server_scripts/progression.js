abf6e0587daba4f53f757a197c0411a4d157626a
mlod1 <kolasmolanskiu123@gmail.com>
2026-05-12 13:44:07 -0400

    Перший запуск: налаштування KubeJS та конфігів

---
+++ kubejs/server_scripts/progression.js
@@ -0,0 +1,25 @@
+﻿// Реєструємо подію для серверних рецептів
+ServerEvents.recipes(event => {
+
+    // 1. Видаляємо стандартний рецепт верстака (Vanilla)
+    event.remove({ output: 'minecraft:crafting_table' })
+
+    // 2. Додаємо новий, складніший рецепт
+    // Використовуємо знання JSON для опису інгредієнтів
+    event.shaped(
+        'minecraft:crafting_table', // Що отримуємо
+        [
+            'AA',
+            'BB'
+        ],
+        {
+            A: '#minecraft:logs',     // Будь-яка колода (тег)
+            B: 'minecraft:flint'      // Кремінь
+        }
+    )
+
+    // Додатково: приклад безформного крафта (Shapeless)
+    // Дозволяємо "розібрати" верстак назад на 4 дошки
+    event.shapeless('4x minecraft:oak_planks', ['minecraft:crafting_table'])
+
+})
